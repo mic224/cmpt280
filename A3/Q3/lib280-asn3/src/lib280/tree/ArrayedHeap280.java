80a914b92a9c6a97cc4d8295c5dae90da5c2b4e0
michael <m.coquet2@gmail.com>
2017-01-28 12:35:16 -0600

    Added all stubs for ArrayedHeap280 A3Q3.

--- A3/Q3/lib280-asn3/src/lib280/tree/ArrayedHeap280.java
+++ A3/Q3/lib280-asn3/src/lib280/tree/ArrayedHeap280.java
@@ -1,11 +1,32 @@
 package lib280.tree;
 
 import lib280.base.Dispenser280;
+import lib280.exception.ContainerFull280Exception;
+import lib280.exception.DuplicateItems280Exception;
+import lib280.exception.NoCurrentItem280Exception;
 
 /**
  * Created by michael on 28/01/17.
  */
-public class ArrayedHeap280 extends ArrayedBinaryTree280 implements Dispenser280 {
+public class ArrayedHeap280<I> extends ArrayedBinaryTree280<I> implements Dispenser280<I> {
 
-    
+
+    public ArrayedHeap280(int cap) {
+        super(cap);
+    }
+
+    @Override
+    public void insert(I x) throws ContainerFull280Exception, DuplicateItems280Exception {
+
+    }
+
+    @Override
+    public void deleteItem() throws NoCurrentItem280Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
