b56fca67406f03a8cdf344d26f39155100a5ae58
michael <m.coquet2@gmail.com>
2017-01-29 15:02:16 -0600

    added stubs for A3Q4b, and finished A3Q4a

--- A3/Q4/SkillTree.java
+++ A3/Q4/SkillTree.java
@@ -3,29 +3,94 @@ import lib280.tree.BasicMAryTree280;
 
 public class SkillTree extends BasicMAryTree280<Skill> {
 
-	/**	
-	 * Create lib280.tree with the specified root node and
-	 * specified maximum arity of nodes.  
-	 * @timing O(1) 
-	 * @param x item to set as the root node
-	 * @param m number of children allowed for future nodes 
-	 */
-	public SkillTree(Skill x, int m)
-	{
-		super(x,m);
-	}
+    /**
+     * Create lib280.tree with the specified root node and
+     * specified maximum arity of nodes.
+     *
+     * @param x item to set as the root node
+     * @param m number of children allowed for future nodes
+     * @timing O(1)
+     */
+    public SkillTree(Skill x, int m) {
+        super(x, m);
+    }
 
-	/**
-	 * A convenience method that avoids typecasts.
-	 * Obtains a subtree of the root.
-	 * 
-	 * @param i Index of the desired subtree of the root.
-	 * @return the i-th subtree of the root.
-	 */
-	public SkillTree rootSubTree(int i) {
-		return (SkillTree)super.rootSubtree(i);
-	}
-	
-	
+    /**
+     * A convenience method that avoids typecasts.
+     * Obtains a subtree of the root.
+     *
+     * @param i Index of the desired subtree of the root.
+     * @return the i-th subtree of the root.
+     */
+    public SkillTree rootSubTree(int i) {
+        return (SkillTree) super.rootSubtree(i);
+    }
+
+    /**
+     * A helpful method for adding subtrees to a skill tree.
+     * Adds a subtree to the root.
+     *
+     * @param tree The root tree.
+     * @param name String name for the new skill to be added to the root.
+     * @param disc String discription of the new skill.
+     * @param cost int Cost of the new skill
+     */
+    public void setTreeNode(SkillTree tree, String name, String disc, int cost) {
+        Skill s = new Skill(name, disc, cost);
+        SkillTree treeNode = new SkillTree(s, tree.futureArity());
+
+        int insertionPosition = tree.rootLastNonEmptyChild() + 1;
+
+        if (insertionPosition > tree.futureArity()) {
+            System.out.println("ERROR.");
+        } else {
+            tree.setRootSubtree(treeNode, insertionPosition);
+        }
+    }
+
+    /**
+     * Lists the dependencies for a given skill.
+     *
+     * @param name String the given skill to list depencies of.
+     * @return LinkedList280 representing all skill depencies of name including name.
+     * @precond the given skill exists.
+     */
+    public LinkedList280<Skill> skillDependencies(String name) {
+        return new LinkedList280<>();
+    }
+
+    public static void main(String[] args) {
+        Skill offense = new Skill("Offense", "Main offensive skills.", 1);
+        SkillTree offenseTree = new SkillTree(offense, 3);
+
+        offenseTree.setTreeNode(offenseTree, "Power", "Power Skills.", 2);
+        offenseTree.setTreeNode(offenseTree, "Speed", "Speed Skills.", 2);
+        offenseTree.setTreeNode(offenseTree, "Accuracy", "Accuracy Skills.", 2);
+
+        offenseTree.rootSubTree(1).setTreeNode(offenseTree.rootSubTree(1), "Bash",
+                "A powerful melee attack.", 3);
+        offenseTree.rootSubTree(1).setTreeNode(offenseTree.rootSubTree(1), "Armor Pierce",
+                "Temporarily reduce enemy armor.", 3);
+        offenseTree.rootSubTree(1).setTreeNode(offenseTree.rootSubTree(1), "Massive Attack",
+                "A massive attack that uses lots of energy.", 3);
+
+        offenseTree.rootSubTree(2).setTreeNode(offenseTree.rootSubTree(2), "Flury",
+                "A series of quick fast attacks.", 3);
+        offenseTree.rootSubTree(2).setTreeNode(offenseTree.rootSubTree(2), "Rebound",
+                "Counter an enemy attack.", 3);
+        offenseTree.rootSubTree(2).setTreeNode(offenseTree.rootSubTree(2), "Scatter Shot",
+                "Do splash damage to multiple enemies", 3);
+
+        offenseTree.rootSubTree(3).setTreeNode(offenseTree.rootSubTree(3), "Eye Gouge",
+                "Quick cheap shot.", 3);
+        offenseTree.rootSubTree(3).setTreeNode(offenseTree.rootSubTree(3), "Target",
+                "Increase hit rate making it easier to hit enemies.", 3);
+        offenseTree.rootSubTree(3).setTreeNode(offenseTree.rootSubTree(3), "Snipe",
+                "Accurate hit.", 3);
+
+
+        
+
+    }
 
 }
