72ac1857d3d7c5d14b26ba433cf9abb34acf3a8a
michael <m.coquet2@gmail.com>
2017-01-28 18:40:56 -0600

    finished insert() method in ArrayedHeap280.

--- A3/Q3/lib280-asn3/src/lib280/tree/ArrayedHeap280.java
+++ A3/Q3/lib280-asn3/src/lib280/tree/ArrayedHeap280.java
@@ -8,16 +8,28 @@ import lib280.exception.NoCurrentItem280Exception;
 /**
  * Created by michael on 28/01/17.
  */
-public class ArrayedHeap280<I> extends ArrayedBinaryTree280<I> implements Dispenser280<I> {
-
+public class ArrayedHeap280<I extends Comparable<? super I>> extends ArrayedBinaryTree280<I> implements Dispenser280<I> {
 
     public ArrayedHeap280(int cap) {
         super(cap);
+        items = (I[]) new Comparable[capacity+1];
     }
 
-    @Override
     public void insert(I x) throws ContainerFull280Exception, DuplicateItems280Exception {
+        if( this.isFull() ) throw new ContainerFull280Exception("Cannot add item to a tree that is full.");
+        else {
+            count++;
+            items[count] = x;
+            currentNode = count;
 
+            if(currentNode > 1) {
+                while (items[findParent(currentNode)].compareTo(items[currentNode]) < 0) {
+                    items[currentNode] = items[findParent(currentNode)];
+                    items[findParent(currentNode)] = x;
+                    currentNode = findParent(currentNode);
+                }
+            }
+        }
     }
 
     @Override
@@ -26,7 +38,44 @@ public class ArrayedHeap280<I> extends ArrayedBinaryTree280<I> implements Dispen
     }
 
     public static void main(String[] args) {
+        System.out.println("Testing initial heap:");
+        ArrayedHeap280<Integer> heap = new ArrayedHeap280<>(16);
 
+        if( (heap == null) || (!heap.isEmpty()) || (heap.capacity() != 16)) {
+            System.out.println("\t Failed.\n");
+        } else {
+            System.out.println("\t Passed.\n");
+        }
+
+        System.out.println("Testing insert():");
+        //generating the heap from tutorial.
+        heap.insert(101);
+        heap.insert(75);
+        heap.insert(88);
+        heap.insert(61);
+        heap.insert(43);
+        heap.insert(67);
+        heap.insert(79);
+        heap.insert(17);
+        heap.insert(3);
+        heap.insert(8);
+        if((heap.isEmpty()) || (heap.count != 10) ) {
+            System.out.println("\t Failed.\n");
+        } else {
+            System.out.println("\t Passed.\n");
+        }
+
+        //inserting offending node.
+        heap.insert(81);
+        if((heap.isEmpty()) || (heap.count != 11) ) {
+            System.out.println("\t Failed.\n");
+        } else {
+            System.out.println("\t Passed.\n");
+        }
+
+
+
+        System.out.println(heap.toString());
     }
 
 }
