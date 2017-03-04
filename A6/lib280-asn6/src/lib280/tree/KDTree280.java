69503f3642dc16282f7fc6d26d883ce20461ec53
michael <m.coquet2@gmail.com>
2017-03-04 13:25:14 -0600

    added all unit tests for KDTree280; assignment 6; starting to build unit tests for partition().

--- A6/lib280-asn6/src/lib280/tree/KDTree280.java
+++ A6/lib280-asn6/src/lib280/tree/KDTree280.java
@@ -1,5 +1,45 @@
 package lib280.tree;
 
-public class KDTree280 {
+import lib280.base.NDPoint280;
 
+public class KDTree280<I extends Comparable<? super I>> {
+
+    /**
+     * Construct a KDTree from an array of KDPoints.
+     * @param pointArray Array of KD points to build the tree from.
+     * @param left offset of start of subarray from which to build a kd-tree
+     * @param right offset of end of subarray from which to build a kd-tree
+     * @param depth the current depth in the partially built tree - note that the root of
+     *              a tree has depth 0 and the k dimensions of the points are numbered 0
+     *              through k-1.
+     */
+    public KDTree280(NDPoint280[] pointArray, int left, int right, int depth) {
+
+    }
+
+    /**
+     * Partition a subarray using its last element as a pivot
+     * @param list array of comparable elements.
+     * @param left lower limit on subarray t e partitioned.
+     * @param right upper limit on subarray to be partitioned.
+     * @return the offset at which the pivot element ended up.
+     */
+    public int partition(I[] list, int left, int right) {
+        return 0;
+    }
+
+    /**
+     * Find the jth smallest item in an array.
+     * @param list array of comparable elements.
+     * @param left offset of start of subarray for which we want the median element.
+     * @param right offset of end of subarray for which we want the median element.
+     * @param j we want to find the element that belongs at array index j.
+     */
+    public void jSmallest(I[] list, int left, int right, int j) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
