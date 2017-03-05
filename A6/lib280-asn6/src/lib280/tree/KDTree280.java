7e323c0eaf79990549d03e4e9299def9e1d24ef4
michael <m.coquet2@gmail.com>
2017-03-05 00:37:41 -0600

    addeaddepassed all unit tests for partition() in KDTree280; assignment 6 question 2.

--- A6/lib280-asn6/src/lib280/tree/KDTree280.java
+++ A6/lib280-asn6/src/lib280/tree/KDTree280.java
@@ -1,7 +1,9 @@
 package lib280.tree;
 
+import com.sun.security.auth.NTDomainPrincipal;
 import lib280.base.NDPoint280;
 import lib280.exception.DuplicateItems280Exception;
+import lib280.exception.InvalidArgument280Exception;
 import lib280.exception.ItemNotFound280Exception;
 
 public class KDTree280<I extends Comparable<? super I>> {
@@ -38,8 +40,24 @@ public class KDTree280<I extends Comparable<? super I>> {
      *                of the elements before and after the pivot.
      * @return the offset at which the pivot element ended up.
      */
-    public int partition(I[] list, int left, int right, int d) {
-        return 0;
+    public int partition(NDPoint280[] list, int left, int right, int d) {
+        double pivot = list[right].idx(d-1);
+
+        int swapOffset = left;
+        for(int i = left; i < right; i++) {
+            if(list[i].idx(d-1) <= pivot) {
+                NDPoint280 temp = list[i];
+                list[i] = list[swapOffset];
+                list[swapOffset] = temp;
+                swapOffset = swapOffset + 1;
+            }
+        }
+
+        NDPoint280 temp = list[right];
+        list[right] = list[swapOffset];
+        list[swapOffset] = temp;
+
+        return swapOffset;
     }
 
     /**
@@ -62,17 +80,21 @@ public class KDTree280<I extends Comparable<? super I>> {
     public static void main(String[] args) {
 
         // first testing partition method
-        System.out.println("\nTesting partition method with non-unique items.");
+        System.out.println("\nTesting partition method.");
 
-        // data for the array of points
+        // data for the array of points(with a duplicate item for testing.
         double[][] dList = new double[][] {
                 {5.0, 2.0},
                 {9.0, 10.0},
                 {11.0, 1.0},
                 {4.0, 3.0},
                 {2.0, 12.0},
-                {5.0, 2.0},
-                {1.0, 5.0}
+                {3.0, 7.0},
+                {1.0, 5.0},
+                {8.0, 20.0},
+                {15.0, 15.0},
+                {29.0, 0.0},
+                {16.0, 16.0}
         };
 
         NDPoint280[] pointArray = new NDPoint280[dList.length];
@@ -82,42 +104,18 @@ public class KDTree280<I extends Comparable<? super I>> {
         }
         KDTree280 tree = new KDTree280();
 
-        int pTest = 0;
-        boolean testing = false;
-        try {
-            pTest = tree.partition(pointArray, 0, dList.length, 1);
-        } catch (DuplicateItems280Exception e) {
-            testing = true;
-        }
-        if(testing) {
-            System.out.println("1: PASS");
-        } else {
-            System.out.println("1: FAIL");
-        }
-
-        // Testing partition with unique items.
-        dList = new double[][] {
-                {5.0, 2.0},
-                {9.0, 10.0},
-                {11.0, 1.0},
-                {4.0, 3.0},
-                {2.0, 12.0},
-                {3.0, 7.0},
-                {1.0, 5.0}
-        };
-
-        pointArray = new NDPoint280[dList.length];
-        // instantiating a new KDTree for testing purposes.
-        for(int i = 0; i < dList.length; i++) {
-            pointArray[i] = new NDPoint280(dList[i]);
-        }
-        tree = new KDTree280();
-
         System.out.println("\nTesting partition() on 1st dimension of unique array: ");
 
-        testing = true;
+        boolean testing = true;
+        int pTest; //store value of pivot from partition()
+
         try {
-            pTest = tree.partition(pointArray, 0, dList.length, 1);
+            pTest = tree.partition(pointArray, 0, dList.length - 1, 1);
+            if(pTest == 9) {
+                System.out.println("1: PASS");
+            } else {
+                System.out.println("1: FAIL");
+            }
         } catch( Exception e) {
             testing = false;
         }
@@ -128,77 +126,127 @@ public class KDTree280<I extends Comparable<? super I>> {
             System.out.println("2: FAIL");
         }
 
-        if(pTest == 0) {
+        pTest = tree.partition(pointArray, 0, dList.length/2, 1);
+
+        if(pTest == 1) {
             System.out.println("3: PASS");
         } else {
             System.out.println("3: FAIL");
         }
 
-        pTest = tree.partition(pointArray, 0, dList.length - 1, 1);
+        pTest = tree.partition(pointArray, dList.length/2, dList.length - 1, 1);
 
-        if(pTest == 2) {
+        if(pTest == 10) {
             System.out.println("4: PASS");
         } else {
             System.out.println("4: FAIL");
         }
 
-        pTest = tree.partition(pointArray, 0, dList.length - 2, 1);
+        pTest = tree.partition(pointArray, dList.length/2+2, dList.length - 1, 1);
 
-        if(pTest == 1) {
+        if(pTest == 10) {
             System.out.println("5: PASS");
         } else {
             System.out.println("5: FAIL");
         }
 
-        pTest = tree.partition(pointArray, 0, 0, 1);
+        pTest = tree.partition(pointArray, 2, 6, 1);
 
-        if(pTest == 4) {
+        if(pTest == 2) {
             System.out.println("6: PASS");
         } else {
             System.out.println("6: FAIL");
         }
 
-        // testing partition on a few elements from 2nd dim.
+        pTest = tree.partition(pointArray, 0, dList.length - 2, 1);
 
-        System.out.println("\nTesting partition() on 2nd dimension of array: ");
-
-        pTest = tree.partition(pointArray, 0, dList.length, 2);
-
-        if(pTest == 3) {
+        if(pTest == 9) {
             System.out.println("7: PASS");
         } else {
             System.out.println("7: FAIL");
         }
 
-        pTest = tree.partition(pointArray, 0, 0, 2);
+        System.out.println("\nTesting partition() on 2nd dimension of unique array: ");
 
-        if(pTest == 1) {
+//        {5.0, 2.0},
+//        {9.0, 10.0},
+//        {11.0, 1.0},
+//        {4.0, 3.0},
+//        {2.0, 12.0},
+//        {3.0, 7.0},
+//        {1.0, 5.0},
+//        {8.0, 20.0},
+//        {15.0, 15.0},
+//        {29.0, 1.0},
+//        {16.0, 16.0}
+
+        testing = true;
+        try {
+            pTest = tree.partition(pointArray, 0, dList.length - 1, 2);
+        } catch( Exception e) {
+            testing = false;
+        }
+
+        if(testing) {
             System.out.println("8: PASS");
         } else {
             System.out.println("8: FAIL");
         }
 
-        pTest = tree.partition(pointArray, 0, dList.length/2, 2);
+        pTest = tree.partition(pointArray, 0, dList.length - 1, 2);
 
-        if(pTest == 2) {
+        if(pTest == 7) {
             System.out.println("9: PASS");
         } else {
             System.out.println("9: FAIL");
         }
 
+        pTest = tree.partition(pointArray, dList.length/2, dList.length - 1, 2);
+
+        if(pTest == 10) {
+            System.out.println("10: PASS");
+        } else {
+            System.out.println("10: FAIL");
+        }
+
+        pTest = tree.partition(pointArray, 3, 7, 2);
+
+        if(pTest == 7) {
+            System.out.println("11: PASS");
+        } else {
+            System.out.println("11: FAIL");
+        }
+
+        pTest = tree.partition(pointArray, 0, 6, 2);
+
+        if(pTest == 1) {
+            System.out.println("12: PASS");
+        } else {
+            System.out.println("12: FAIL");
+        }
+
+        pTest = tree.partition(pointArray, 0, dList.length - 2, 2);
+
+        if(pTest == 9) {
+            System.out.println("13: PASS");
+        } else {
+            System.out.println("13: FAIL");
+        }
+
+
         System.out.println("\nTesting partition() on higher dimension than array: ");
 
         testing = false;
         try {
-            pTest = tree.partition(pointArray, 0, dList.length, 3);
-        } catch(ItemNotFound280Exception e) {
+            pTest = tree.partition(pointArray, 0, dList.length - 1, 3);
+        } catch(InvalidArgument280Exception e) {
             testing = true;
         }
 
         if(testing) {
-            System.out.println("10: PASS");
+            System.out.println("14: PASS");
         } else {
-            System.out.println("10: FAIL");
+            System.out.println("14: FAIL");
         }
 
     }
