74dd3e6421c7c9c6417b4d794cf2402547f6e908
michael <m.coquet2@gmail.com>
2017-03-05 13:59:27 -0600

    Finished all unit testing and passed all unit tests for KDTree280.jSmallest(); A6Q2.

--- A6/lib280-asn6/src/lib280/tree/KDTree280.java
+++ A6/lib280-asn6/src/lib280/tree/KDTree280.java
@@ -1,12 +1,9 @@
 package lib280.tree;
 
-import com.sun.security.auth.NTDomainPrincipal;
 import lib280.base.NDPoint280;
-import lib280.exception.DuplicateItems280Exception;
 import lib280.exception.InvalidArgument280Exception;
-import lib280.exception.ItemNotFound280Exception;
 
-public class KDTree280<I extends Comparable<? super I>> {
+public class KDTree280 {
 
     /**
      * Construct an empty KDTree280, just for testing purposes.
@@ -17,12 +14,13 @@ public class KDTree280<I extends Comparable<? super I>> {
 
     /**
      * Construct a KDTree from an array of KDPoints.
+     *
      * @param pointArray Array of KD points to build the tree from.
-     * @param left offset of start of subarray from which to build a kd-tree
-     * @param right offset of end of subarray from which to build a kd-tree
-     * @param depth the current depth in the partially built tree - note that the root of
-     *              a tree has depth 0 and the k dimensions of the points are numbered 0
-     *              through k-1.
+     * @param left       offset of start of subarray from which to build a kd-tree
+     * @param right      offset of end of subarray from which to build a kd-tree
+     * @param depth      the current depth in the partially built tree - note that the root of
+     *                   a tree has depth 0 and the k dimensions of the points are numbered 0
+     *                   through k-1.
      */
     public KDTree280(NDPoint280[] pointArray, int left, int right, int depth) {
 
@@ -30,22 +28,23 @@ public class KDTree280<I extends Comparable<? super I>> {
 
     /**
      * Partition a subarray using its last element as a pivot
-     * @param list array of comparable elements.
-     * @param left lower limit on subarray t e partitioned.
+     *
+     * @param list  array of comparable elements.
+     * @param left  lower limit on subarray t e partitioned.
      * @param right upper limit on subarray to be partitioned.
+     * @return the offset at which the pivot element ended up.
      * @Precondition all elements in ’list’ are unique
      * @Postcondition all elements smaller than the pivot appear in the leftmost part
-     *                of the subarray, then the pivot element, followed by the elements
-     *                larger than the pivot. There is no guarantee about the ordering
-     *                of the elements before and after the pivot.
-     * @return the offset at which the pivot element ended up.
+     * of the subarray, then the pivot element, followed by the elements
+     * larger than the pivot. There is no guarantee about the ordering
+     * of the elements before and after the pivot.
      */
     public int partition(NDPoint280[] list, int left, int right, int d) {
-        double pivot = list[right].idx(d-1);
+        double pivot = list[right].idx(d - 1);
 
         int swapOffset = left;
-        for(int i = left; i < right; i++) {
-            if(list[i].idx(d-1) <= pivot) {
+        for (int i = left; i < right; i++) {
+            if (list[i].idx(d - 1) <= pivot) {
                 NDPoint280 temp = list[i];
                 list[i] = list[swapOffset];
                 list[swapOffset] = temp;
@@ -62,28 +61,50 @@ public class KDTree280<I extends Comparable<? super I>> {
 
     /**
      * Find the jth smallest item in an array.
-     * @param list array of comparable elements.
-     * @param left offset of start of subarray for which we want the median element.
+     *
+     * @param list  array of comparable elements.
+     * @param left  offset of start of subarray for which we want the median element.
      * @param right offset of end of subarray for which we want the median element.
-     * @param j we want to find the element that belongs at array index j.
+     * @param j     we want to find the element that belongs at array index j.
      * @Precondition left <= j <= right
      * @Precondition all elements in 'list' are unique
      * @Postcondition the element x that belongs at offseet j, if the subarray were
-     *                sorted, is at offset j. Elements in the subarray smaller than
-     *                x are to the left of offset j and the elements in the subarray
-     *                larger than x are to the right of offset j.
+     * sorted, is at offset j. Elements in the subarray smaller than
+     * x are to the left of offset j and the elements in the subarray
+     * larger than x are to the right of offset j.
      */
-    public void jSmallest(I[] list, int left, int right, int j) {
+    public void jSmallest(NDPoint280[] list, int left, int right, int j, int d) {
+        if (right > left) {
+//      Partition  the  subarray  using  the  last  element , list[right], as a pivot.
+//      The  index of the  pivot  after  partitioning  is  returned.
+//      This is  exactly  the  same  partition  algorithm  used by  quicksort.
+            int pivotIndex = partition(list, left, right, d);
 
+//           if the pivotIndex is equal to j, then we found the j-th smallest
+//           element and it is in the right place
+
+//           If the position j is smaller than the pivotIndex, we know that the jth
+//           smallest element must be between left, and pivotIndex-1, so recursively
+//           look for the jth smallest element in that subarray.
+            if(j < pivotIndex) {
+                jSmallest(list, left, pivotIndex - 1, j, d);
+            } else {
+//           Otherwise, the position j must be larger than the pivotIndex, so the
+//           jth smallest element must be between pivotIndex + 1 and right
+                jSmallest(list, pivotIndex + 1, right, j, d);
+            }
+//           Otherwise, the pivot ended up at list[j], and the pivot is the jth smallest
+//           element.
+        }
     }
 
     public static void main(String[] args) {
 
-        // first testing partition method
+//      first testing partition method
         System.out.println("\nTesting partition method.");
 
-        // data for the array of points(with a duplicate item for testing.
-        double[][] dList = new double[][] {
+//      data for the array of points(with a duplicate item for testing.
+        double[][] dList = new double[][]{
                 {5.0, 2.0},
                 {9.0, 10.0},
                 {11.0, 1.0},
@@ -98,8 +119,8 @@ public class KDTree280<I extends Comparable<? super I>> {
         };
 
         NDPoint280[] pointArray = new NDPoint280[dList.length];
-        // instantiating a new KDTree for testing purposes.
-        for(int i = 0; i < dList.length; i++) {
+//      instantiating a new KDTree for testing purposes.
+        for (int i = 0; i < dList.length; i++) {
             pointArray[i] = new NDPoint280(dList[i]);
         }
         KDTree280 tree = new KDTree280();
@@ -111,40 +132,40 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         try {
             pTest = tree.partition(pointArray, 0, dList.length - 1, 1);
-            if(pTest == 9) {
+            if (pTest == 9) {
                 System.out.println("1: PASS");
             } else {
                 System.out.println("1: FAIL");
             }
-        } catch( Exception e) {
+        } catch (Exception e) {
             testing = false;
         }
 
-        if(testing) {
+        if (testing) {
             System.out.println("2: PASS");
         } else {
             System.out.println("2: FAIL");
         }
 
-        pTest = tree.partition(pointArray, 0, dList.length/2, 1);
+        pTest = tree.partition(pointArray, 0, dList.length / 2, 1);
 
-        if(pTest == 1) {
+        if (pTest == 1) {
             System.out.println("3: PASS");
         } else {
             System.out.println("3: FAIL");
         }
 
-        pTest = tree.partition(pointArray, dList.length/2, dList.length - 1, 1);
+        pTest = tree.partition(pointArray, dList.length / 2, dList.length - 1, 1);
 
-        if(pTest == 10) {
+        if (pTest == 10) {
             System.out.println("4: PASS");
         } else {
             System.out.println("4: FAIL");
         }
 
-        pTest = tree.partition(pointArray, dList.length/2+2, dList.length - 1, 1);
+        pTest = tree.partition(pointArray, dList.length / 2 + 2, dList.length - 1, 1);
 
-        if(pTest == 10) {
+        if (pTest == 10) {
             System.out.println("5: PASS");
         } else {
             System.out.println("5: FAIL");
@@ -152,7 +173,7 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         pTest = tree.partition(pointArray, 2, 6, 1);
 
-        if(pTest == 2) {
+        if (pTest == 2) {
             System.out.println("6: PASS");
         } else {
             System.out.println("6: FAIL");
@@ -160,7 +181,7 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         pTest = tree.partition(pointArray, 0, dList.length - 2, 1);
 
-        if(pTest == 9) {
+        if (pTest == 9) {
             System.out.println("7: PASS");
         } else {
             System.out.println("7: FAIL");
@@ -168,26 +189,14 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         System.out.println("\nTesting partition() on 2nd dimension of unique array: ");
 
-//        {5.0, 2.0},
-//        {9.0, 10.0},
-//        {11.0, 1.0},
-//        {4.0, 3.0},
-//        {2.0, 12.0},
-//        {3.0, 7.0},
-//        {1.0, 5.0},
-//        {8.0, 20.0},
-//        {15.0, 15.0},
-//        {29.0, 1.0},
-//        {16.0, 16.0}
-
         testing = true;
         try {
             pTest = tree.partition(pointArray, 0, dList.length - 1, 2);
-        } catch( Exception e) {
+        } catch (Exception e) {
             testing = false;
         }
 
-        if(testing) {
+        if (testing) {
             System.out.println("8: PASS");
         } else {
             System.out.println("8: FAIL");
@@ -195,15 +204,15 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         pTest = tree.partition(pointArray, 0, dList.length - 1, 2);
 
-        if(pTest == 7) {
+        if (pTest == 7) {
             System.out.println("9: PASS");
         } else {
             System.out.println("9: FAIL");
         }
 
-        pTest = tree.partition(pointArray, dList.length/2, dList.length - 1, 2);
+        pTest = tree.partition(pointArray, dList.length / 2, dList.length - 1, 2);
 
-        if(pTest == 10) {
+        if (pTest == 10) {
             System.out.println("10: PASS");
         } else {
             System.out.println("10: FAIL");
@@ -211,7 +220,7 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         pTest = tree.partition(pointArray, 3, 7, 2);
 
-        if(pTest == 7) {
+        if (pTest == 7) {
             System.out.println("11: PASS");
         } else {
             System.out.println("11: FAIL");
@@ -219,7 +228,7 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         pTest = tree.partition(pointArray, 0, 6, 2);
 
-        if(pTest == 1) {
+        if (pTest == 1) {
             System.out.println("12: PASS");
         } else {
             System.out.println("12: FAIL");
@@ -227,7 +236,7 @@ public class KDTree280<I extends Comparable<? super I>> {
 
         pTest = tree.partition(pointArray, 0, dList.length - 2, 2);
 
-        if(pTest == 9) {
+        if (pTest == 9) {
             System.out.println("13: PASS");
         } else {
             System.out.println("13: FAIL");
@@ -239,15 +248,63 @@ public class KDTree280<I extends Comparable<? super I>> {
         testing = false;
         try {
             pTest = tree.partition(pointArray, 0, dList.length - 1, 3);
-        } catch(InvalidArgument280Exception e) {
+        } catch (InvalidArgument280Exception e) {
             testing = true;
         }
 
-        if(testing) {
+        if (testing) {
             System.out.println("14: PASS");
         } else {
             System.out.println("14: FAIL");
         }
 
+//      Starting testing for jSmallest()
+//      restoring original array of NDPOints280 for testing
+
+        System.out.println("\nTesting jSmallest(): ");
+
+        System.out.println("\nTesting jSmallest() on 1st dimension: ");
+        for (int i = 0; i < dList.length; i++) {
+            pointArray[i] = new NDPoint280(dList[i]);
+        }
+
+        tree.jSmallest(pointArray, 0, dList.length - 1, dList.length / 2, 1);
+        if (pointArray[pointArray.length / 2].idx(0) == 8) {
+            System.out.println("15: PASS");
+        } else {
+            System.out.println("15: FAIL");
+        }
+
+        tree.jSmallest(pointArray, 0, 3, 2, 1);
+        if (pointArray[2].idx(0) == 4) {
+            System.out.println("16: PASS");
+        } else {
+            System.out.println("16: FAIL");
+        }
+
+        tree.jSmallest(pointArray, 6, dList.length-1, 7, 1);
+        if (pointArray[7].idx(0) == 11) {
+            System.out.println("17: PASS");
+        } else {
+            System.out.println("17: FAIL");
+        }
+
+        System.out.println("\nTesting jSmallest() on 2nd dimension.");
+
+        tree.jSmallest(pointArray, 0, dList.length-1, dList.length/2, 2);
+        if(pointArray[dList.length/2].idx(1) == 7) {
+            System.out.println("18: PASS");
+        } else {
+            System.out.println("18: FAIL");
+        }
+
+        tree.jSmallest(pointArray, dList.length/2, dList.length - 1, 8, 2);
+        if(pointArray[8].idx(1) == 15) {
+            System.out.println("19: PASS");
+        } else {
+            System.out.println("19: FAIL");
+        }
+
+        
     }
 }
